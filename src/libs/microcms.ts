458b7b240fc63965ab0f59c4ddc5b5fba7801dc4
NzzyYkky <62602398+NzzyYkky@users.noreply.github.com>
2023-05-14 22:15:08 +0900

    add news feature

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -1,10 +1,21 @@
-import { createClient, MicroCMSQueries } from "microcms-js-sdk"
+import { createClient, MicroCMSQueries } from 'microcms-js-sdk';
 
 const client = createClient({
-   serviceDomain: import.meta.env.SERVICE_DOMAIN,
-   apiKey: import.meta.env.API_KEY,
+	serviceDomain: import.meta.env.SERVICE_DOMAIN,
+	apiKey: import.meta.env.API_KEY,
 });
 
 export const getNews = async (queries: MicroCMSQueries) => {
-   return await client.get({ endpoint: 'news', queries})
-}
+	return await client.get({ endpoint: 'news', queries });
+};
+
+export const getNewsDetail = async (
+	newsid: string,
+	queries?: MicroCMSQueries
+) => {
+	return await client.getListDetail({
+		endpoint: 'news',
+		contentId: newsid,
+		queries,
+	});
+};
